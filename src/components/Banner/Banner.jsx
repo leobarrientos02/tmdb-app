7706b5da4eecd8e4e69e115374abd4bf7097433c
leobarrientos02 <leobarrientos02@gmail.com>
2023-02-25 15:24:56 -0500

    Search is Wokring
    Almost done need to relax to think

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -15,64 +15,8 @@ export function Banner() {
         <h3 className="text-3xl font-semibold">
           Explore our large variety of movies.
         </h3>
+        <Search location={"banner"} />
       </div>
-
-      <Search location={"banner"} />
-      {/* <form onSubmit={submitHandler} className="search">
-        <div
-          className="search-type-wrapper"
-          onMouseEnter={() =>
-            handleMouseEnter("banner-search-options", "search-banner-arrow")
-          }
-          onMouseLeave={() =>
-            handleMouseLeave("banner-search-options", "search-banner-arrow")
-          }
-        >
-          <div className="search-type">
-            <p>{checkType(type)}</p>
-            <FiChevronDown
-              size="1.2em"
-              id="search-banner-arrow"
-              className="arrow"
-            />
-          </div>
-          <div className="search-types" id="banner-search-options">
-            <p
-              onClick={() => setType("movie")}
-              className={type === "movie" ? "hide" : ""}
-            >
-              Movies
-            </p>
-            <p
-              onClick={() => setType("tv")}
-              className={type === "tv" ? "hide" : ""}
-            >
-              Shows
-            </p>
-            <p
-              onClick={() => setType("company")}
-              className={type === "company" ? "hide" : ""}
-            >
-              Company
-            </p>
-            <p
-              onClick={() => setType("person")}
-              className={type === "person" ? "hide" : ""}
-            >
-              Person
-            </p>
-          </div>
-        </div>
-        <input
-          className="search-input"
-          type="text"
-          required
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          placeholder={`Search for ${checkType(type)}`}
-        />
-        <input className="search-btn" type="submit" value="Search" />
-      </form> */}
     </motion.div>
   );
 }
